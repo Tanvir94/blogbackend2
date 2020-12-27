141c86d81c47bb51c82b5a5247438b51d32fc76d
Tanvir <tave_94@hotmail.com>
2020-12-27 20:58:25 +0100

    staged changes

--- blogbackend2/config/db.confiq.js
+++ blogbackend2/config/db.confiq.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost:27017/bezkoder_db"
+    url:" mongodb://m001-student:m001-mongodb-basics@cluster0-shard-00-00.pomof.mongodb.net:27017,cluster0-shard-00-01.pomof.mongodb.net:27017,cluster0-shard-00-02.pomof.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-6x19ex-shard-0&authSource=admin&retryWrites=true&w=majority"
   };
